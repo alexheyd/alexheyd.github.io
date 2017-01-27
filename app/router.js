db0be97ab1a778b0002b0545c3630e9701007231
Alex Heyd <alex@gaikai.com>
2017-01-26 16:30:32 -0800

    Added placeholder nav component and basic route structure.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,21 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('resume');
+
+  this.route('portfolio', function() {
+    this.route('view', { path: 'view/:id' });
+  });
+
+  this.route('code-samples', function() {
+    this.route('view', { path: 'view/:id' });
+  });
+
+  this.route('projects', function() {
+    this.route('view', { path: 'view/:id' });
+  });
+
+  this.route('contact');
 });
 
 export default Router;
